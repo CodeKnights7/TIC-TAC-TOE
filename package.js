e05027d4ade3230915dba9087da1b0d9f1159e9b
Karthik Kallapiran.T <116157045+CodeKnights7@users.noreply.github.com>
2023-12-18 23:01:21 +0530

    Add files via upload

---
+++ package.js
@@ -0,0 +1,12 @@
+const stack = {
+	uid:56375637,
+	title:"apple macbook pro",
+	price:"$2300",
+	processor:"apple silicon chip",
+}
+
+
+
+
+
+module.exports = stack
---
+++ script.js
@@ -0,0 +1,5 @@
+import './package.js'
+
+console.log("Welcome")
+
+console.log(''); 
